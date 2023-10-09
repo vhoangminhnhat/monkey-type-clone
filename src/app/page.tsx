1e242da6650def5e1bec8f3e1888f0dc308d624e
bnm55vnn <90960555+vhoangminhnhat@users.noreply.github.com>
2023-10-09 17:41:56 +0700

    Creating database connection

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,34 @@
-"use client"
-import Image from 'next/image'
+"use client";
+import Image from "next/image";
+import "./style.scss"
 
 export default function Home() {
   return (
-    <div>
-      èdsfdfdsfsfsf
+    <div style={{display: "grid", placeItems: "center"}}>
+      <h1>
+      <span>W</span>
+      <span>e</span>
+      <span>l</span>
+      <span>c</span>
+      <span>o</span>
+      <span>m</span>
+      <span>e</span>
+      &nbsp;
+      <span>T</span>
+      <span>o</span>
+      &nbsp;
+      <span>M</span>
+      <span>o</span>
+      <span>n</span>
+      <span>k</span>
+      <span>e</span>
+      <span>y</span>
+      <span>T</span>
+      <span>y</span>
+      <span>p</span>
+      <span>e</span>
+    </h1>
     </div>
-  )
+    
+  );
 }
